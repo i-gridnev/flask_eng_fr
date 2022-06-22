c4e58544935f89c7dcb6df409b1b406a26ff3e5c
Ilia Gridnev <mr.igridnev@gmail.com>
2022-06-22 09:48:28 +0000

    added some tests

--- final_project/tests/tests.py
+++ final_project/tests/tests.py
@@ -12,11 +12,19 @@ class TestTranslation(unittest.TestCase):
         fr_text = 'Bonjour'
         self.assertEqual(english_to_french(en_text), fr_text)
 
+    def test_english_to_french_not_equal(self):
+        ''' Not equal en->fr '''
+        self.assertNotEqual(english_to_french('Something'), 'Anything')
+
     def test_french_to_english(self):
         ''' Basic fr->en '''
         en_text = 'Hello'
         fr_text = 'Bonjour'
         self.assertEqual(french_to_english(fr_text), en_text)
+
+    def test_french_to_english_not_equal(self):
+        ''' Not equal fr->en '''
+        self.assertNotEqual(french_to_english('Something'), 'Anything')
 
 
 if __name__ == '__main__':
